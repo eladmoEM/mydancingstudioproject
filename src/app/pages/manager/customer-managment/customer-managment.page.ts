fad64ec75c10fd01a314e0f57c832e96fe65b15c
Elad Moalem <116482312+eladmoEM@users.noreply.github.com>
2023-07-08 16:44:10 +0300

    update 16:45

--- src/app/pages/manager/customer-managment/customer-managment.page.ts
+++ src/app/pages/manager/customer-managment/customer-managment.page.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-managment.page.scss'],
 })
 export class CustomerManagmentPage implements OnInit {
+  customers: any[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.customers = [
+      {
+        parentsNames: 'John and Jane Doe',
+        username: 'jdoe',
+        password: 'password123',
+        email: 'jdoe@example.com',
+        phoneNumber: '1234567890',
+        numberOfChildren: 2,
+        childName: 'Child Name',
+        childID: '1',
+        gender: 'Male',
+        courseType: 'Type1',
+        birthdate: '2005-01-01',
+        teacher: 'Teacher Name'
+      }
+    ];
   }
 
 }
